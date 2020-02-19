595918beaa51e274d8e3776a32f67fc9cce5a339
Nathan Roy <nathanmroy@yahoo.com>
2020-02-18 20:33:08 -0800

    added article table

--- db.js
+++ db.js
@@ -4,25 +4,37 @@ const client = new pg.Client('postgress://localhost/crud');
 
 client.connect();
 
-const sync = async() => {
-    const SQL = `
+const sync = async () => {
+	const SQL = `
     CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+    DROP TABLE IF EXISTS articles;
     DROP TABLE IF EXISTS authors;
+
     CREATE TABLE authors(
+        id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
         first_name VARCHAR(100),
         last_name VARCHAR(100),
-        date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        id UUID PRIMARY KEY DEFAULT uuid_generate_v4()     
+        date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
-    
+
+    CREATE TABLE articles(
+        id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+        title VARCHAR(255),
+        body TEXT,
+        date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        author_id UUID REFERENCES authors(id)
+    );
+
     INSERT INTO authors(first_name, last_name) 
-        VALUES( 'Teddy', 'Roosevelt')
-        
-    `;
-    
-    await client.query(SQL);
+        VALUES( 'Teddy', 'Roosevelt');
+
+        INSERT INTO articles(title, body, author_id)
+        VALUES( 'The Vigorous Life', 'Dee-lighted!', (SELECT id FROM authors WHERE first_name = 'Teddy'));
+        `;
+
+	await client.query(SQL);
 };
 
 module.exports = {
-    sync
-}
+	sync
+};
